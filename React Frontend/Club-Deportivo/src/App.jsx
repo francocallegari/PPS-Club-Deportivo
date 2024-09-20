3df2e81e0ecb56fef0bcbd52bf25d1bd54c7e8b9
AnaRustici <anabellarustici@gmail.com>
2024-09-20 18:13:57 -0300

    created initial page

--- React Frontend/Club-Deportivo/src/App.jsx
+++ React Frontend/Club-Deportivo/src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import MainLayout from './components/mainLayout/MainLayout'
+import Dashboard from './components/dashboard/Dashboard'
 import './App.css'
+import Sports from './components/sports/Sports'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <MainLayout>
+          <Dashboard></Dashboard>
+        </MainLayout>
+      )
+    },
+    {
+      path: "/Sports",
+      element: (
+        <MainLayout>
+          <Sports></Sports>
+        </MainLayout>
+      )
+    }
+  ])
   return (
-    <>
-
-    </>
+    <RouterProvider router={router}></RouterProvider>
   )
 }
 
--- React Frontend/Club-Deportivo/src/components/dashboard/Dashboard.jsx
+++ React Frontend/Club-Deportivo/src/components/dashboard/Dashboard.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Button, Container, Image } from 'react-bootstrap'
+import img_deportes from '../../images/deportes.jpeg'
+import img_noticias from '../../images/noticias.jpeg'
+import img_actividades from '../../images/actividades.jpeg'
+import './Dashboard.css'
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const handleSportsNavigation = (e) => {
+    e.preventDefault();
+    navigate('/Sports');
+  }
+
   return (
-    <div>Dashboard</div>
+    <div className='main-div'>
+      <Container className='seccion-container' onClick={handleSportsNavigation}>
+        <h2>Deportes</h2>
+        <Image src={img_deportes} height='170px' style={{borderRadius:'20px'}}></Image>
+        <p>Revisa los distintos deportes en los que podés participar</p>
+      </Container>
+      <Container className='seccion-container'>
+        <h2>Noticias</h2>
+        <Image src={img_noticias} height='170px' style={{borderRadius:'20px'}}></Image>
+        <p>Revisa las noticias relacionadas al Club</p>
+      </Container>
+      <Container className='seccion-container'>
+        <h2>Actividades</h2>
+        <Image src={img_actividades} height='170px' width='320px' style={{borderRadius:'20px'}}></Image>
+        <p>¡Revisa las actividades disponibles a las que podés anotarte!</p>
+      </Container>
+    </div>
   )
 }
 
