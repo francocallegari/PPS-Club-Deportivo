9cd4010e8bd8dcae2a3d14a1435899fb74a2c666
facundolgomez <facundolgomez87@gmail.com>
2024-09-20 20:29:42 -0300

    added director interface

---
+++ ClubApi/Application/Interfaces/IDirectorService.cs
@@ -0,0 +1,21 @@
+﻿using Application.Models.Request;
+using Application.Models.Response;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IDirectorService
+    {
+        ICollection<UserResponse> GetAllDirectors();
+        UserResponse GetDirectorById(int id);
+        void UpdateDirector(int id, UserRequest directorRequest);
+        void DeleteDirector(int id);
+        void CreateDirector(UserRequest directorRequest);
+
+    }
+}
