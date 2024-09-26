1e73a7384e4d32c45b2f0ac88926169de2221c67
ManueldeMacedo <75130351+ManueldeMacedo@users.noreply.github.com>
2024-09-25 22:35:08 -0300

    Sports controller added

--- ClubApi/Application/Models/SportDto.cs
+++ ClubApi/Application/Models/SportDto.cs
@@ -24,18 +24,25 @@ namespace Application.Models
             dto.Members = MemberDto.CreateList(sport.Members);
 
             return dto;
-
         }
 
         public static List<SportDto> CreateList(List<Sport> sports)
         {
             List<SportDto> listDto = [];
             foreach (var s in sports)
-            {
                 listDto.Add(Create(s));
-            }
 
             return listDto;
+        }
+
+        public static Sport From(SportDto dto)
+        {
+            var sport      = new Sport();
+            sport.Id       = dto.Id;
+            sport.Name     = dto.Name;
+            sport.Capacity = dto.Capacity;
+
+            return sport;
         }
     }
 }
---
+++ ClubApi/ClubApi/Controllers/SportsController.cs
@@ -0,0 +1,99 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using Application.Models.Request;
+using Application.Models.Response;
+using Application.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace ClubApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SportsController : ControllerBase
+    {
+        private readonly ISportsService _sportsService;
+
+        public SportsController(ISportsService sportsService)
+        {
+            _sportsService = sportsService;
+        }
+
+        //[HttpGet]
+        //public ActionResult<ICollection<SportDto>> GetAllSports()
+        //{
+        //    try
+        //    {
+        //        return Ok(_sportsService.GetAllSports());
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError,
+        //            "Ha ocurrido un error inesperado. Error: " + ex.Message);
+        //    }
+        //}
+
+        [HttpGet("{name}")]
+        public ActionResult<SportDto> GetSportByName(string name)
+        {
+            try
+            {
+                return Ok(_sportsService.GetSportByName(name));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ha ocurrido un error inesperado. Error: " + ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<ICollection<Member>> GetAllMembers()
+        {
+            try
+            {
+                return Ok(_sportsService.GetAllMembers());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Ha ocurrido un error inesperado. Error: " + ex.Message);
+            }
+        }
+
+        //[HttpPost]
+        //public ActionResult<SportDto> CreateSport([FromBody] SportDto sport)
+        //{
+        //    try
+        //    {
+        //        var existingSport = _sportsService.GetSportByName(sport.Name);
+
+        //        if (existingSport != null)
+        //            return BadRequest("El deporte ya existe.");
+
+        //        return Ok(_sportsService.CreateSport(sport));
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError,
+        //            "Ha ocurrido un error inesperado. Error: " + ex.Message);
+        //    }
+        //}
+
+        //[HttpDelete("{id}")]
+        //public IActionResult DeleteSport([FromRoute] int id)
+        //{
+        //    try
+        //    {
+        //        _sportsService.DeleteSport(id);
+        //        return NoContent();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError,
+        //            "Ha ocurrido un error inesperado. Error: " + ex.Message);
+        //    }
+        //}
+    }
+}
