3f285a8ce802a512227b0e2c0b5f3733609d9c74
ManueldeMacedo <75130351+ManueldeMacedo@users.noreply.github.com>
2024-09-25 21:03:23 -0300

    User repository adjustments

--- ClubApi/Infrastructure/Data/RepositoryUser.cs
+++ ClubApi/Infrastructure/Data/RepositoryUser.cs
@@ -1,10 +1,6 @@
 ﻿using Domain.Entities;
 using Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
@@ -28,6 +24,35 @@ namespace Infrastructure.Data
             var user = _context.Users.FirstOrDefault(x => x.Email == email)
                 ?? throw new Exception("Usuario no encontrado");
             return user;
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task DeleteAsync(User user)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<List<User>> ListAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
         }
     }
 }
