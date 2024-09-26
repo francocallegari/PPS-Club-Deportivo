9a5da79ca6e00a43d9df68e6e81785eda0db5f24
facundolgomez <facundolgomez87@gmail.com>
2024-09-25 21:43:36 -0300

    implemented the method of approving events

--- ClubApi/Application/Services/DirectorService.cs
+++ ClubApi/Application/Services/DirectorService.cs
@@ -1,17 +1,20 @@
 ﻿using Application.Interfaces;
 using Application.Models.Request;
 using Application.Models.Response;
+using Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Application.Services
 {
     public class DirectorService : IDirectorService
     {
         private readonly IUserService _userService;
+        private readonly IEventService _eventService;
 
         public DirectorService(IUserService userService)
         {
@@ -58,6 +61,32 @@ namespace Application.Services
                 throw new InvalidOperationException("El tipo de usuario debe ser Director.");
 
             _userService.CreateUser(directorRequest);
+        }
+
+        public void ApproveEvents(int directorId, List<int> eventIds)
+        {
+            var director = _userService.GetUserById(directorId);
+
+            
+            if (!director.UserType.Equals("Director", StringComparison.OrdinalIgnoreCase))
+                throw new InvalidOperationException("Solo los directores pueden aprobar eventos.");
+
+            
+            var eventsToApprove = _eventService.GetEventsByIds(eventIds)
+                .Where(e => e.Status == EventStatus.Pending &&
+                            _userService.GetUserByUserName(e.CreatedBy).UserType == "Administrator")
+                .ToList();
+
+            if (!eventsToApprove.Any())
+                throw new InvalidOperationException("No hay eventos válidos para aprobar.");
+
+            
+            foreach (var ev in eventsToApprove)
+            {
+                ev.Status = EventStatus.Approved;
+                ev.ApprovedBy = director.UserName;  
+                _eventService.UpdateEvent(ev);
+            }
         }
     }
 }
