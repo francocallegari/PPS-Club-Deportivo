60c454c91adea1e7eb38cea30d2953120c39fc15
francocallegari <francocallegari12@gmail.com>
2024-10-01 17:09:17 -0300

    finish payment metohd

--- React Frontend/Club-Deportivo/src/components/paymentMethod/PaymentMethod.jsx
+++ React Frontend/Club-Deportivo/src/components/paymentMethod/PaymentMethod.jsx
@@ -1,6 +1,8 @@
 import { React, useState } from "react";
 import axios from "axios";
 import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import "./PaymentMethod.css";
+import { Button } from "react-bootstrap";
 
 const PaymentMethod = () => {
   const [preferenceId, setPreferenceId] = useState(null);
@@ -16,41 +18,40 @@ const PaymentMethod = () => {
     setError(null);
     try {
       const response = await axios.post(
-        "https://localhost:7081/Payment/create-preference", // Cambiar el guion bajo por un guion
+        "https://localhost:7081/Payment/create-preference",
         {
           title: "Pago membresía",
           quantity: 1,
           price: 100,
-          currencyId: "ARS", // Agregar currencyId si es necesario
+          currencyId: "ARS",
           successUrl: "http://google.com/",
           failureUrl: "http://google.com/",
           pendingUrl: "http://google.com/",
         }
       );
 
-      const { id } = response.data; // Obtén el ID de la respuesta
-      setPreferenceId(id); // Almacena el ID de la preferencia
+      const { id } = response.data;
+      setPreferenceId(id);
     } catch (error) {
       setError("Error al crear la preferencia");
       console.error(error);
     } finally {
-      setLoading(false); // Asegúrate de detener el loading en cualquier caso
+      setLoading(false);
     }
   };
 
   const handlePay = async () => {
-    await createPreference(); // Llama a la función para crear la preferencia
+    await createPreference();
   };
 
   return (
     <div className="payment-method">
       <div className="card-product">
         <div className="card">
-          <h3>Mercado Pago</h3>
-          <p className="price">$$$</p>
-          <button onClick={handlePay} disabled={loading}>
+          <h3>Pagar con Mercado Pago</h3>
+          <Button variant="primary" onClick={handlePay} disabled={loading}>
             {loading ? "Cargando..." : "Pagar"}
-          </button>
+          </Button>
           {error && <p className="error">{error}</p>}
           {preferenceId && (
             <Wallet
