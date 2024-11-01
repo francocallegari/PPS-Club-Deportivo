d827c96911c3865df9ad1d8bb768c1bc63bc28c2
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2024-10-31 21:59:21 -0300

    another route added

--- ClubApi/ClubApi/Program.cs
+++ ClubApi/ClubApi/Program.cs
@@ -59,7 +59,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("http://localhost:5173")
+            builder.WithOrigins("http://localhost:5173", "https://localhost:7062")
                    .AllowAnyHeader()
             .AllowAnyMethod();
         });
