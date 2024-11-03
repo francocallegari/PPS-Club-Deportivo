d3b69ecdaf3dd95a7aef5fa142fb9cf930fa098a
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2024-11-03 13:37:17 -0300

    sport

--- React Frontend/Club-Deportivo/src/components/sportDetail/SportDetail.jsx
+++ React Frontend/Club-Deportivo/src/components/sportDetail/SportDetail.jsx
@@ -1,75 +1,107 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Modal, Button, Table } from "react-bootstrap";
 import "./SportDetail.css";
 import { AuthenticationContext } from "../../services/authentication/AuthenticationContext";
 
 const SportDetail = ({ sport, onClose }) => {
-
   const { user } = useContext(AuthenticationContext);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const handleInscripcion = async () => {
+    try {
+      const response = await fetch(`https://localhost:7081/api/TrainingSession/`, {
+        method: "GET",
+        headers: {
+          accept: "*/*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: user.id,
+          sportId: sport.id
+        })
+      });
+
+      if (response.ok) {
+        setShowConfirmation(true);
+      } else {
+        throw new Error("Error al realizar la inscripción");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  const handleCloseConfirmation = () => {
+    setShowConfirmation(false);
+  };
 
   return (
-    <Modal
-      show={!!sport}
-      onHide={onClose}
-      centered
-      className="sport-detail-modal"
-    >
-      <Modal.Header closeButton className="sport-title">
-        <Modal.Title>{sport?.name}</Modal.Title>
-      </Modal.Header>
+    <>
+      <Modal
+        show={!!sport}
+        onHide={onClose}
+        centered
+        className="sport-detail-modal"
+      >
+        <Modal.Header closeButton className="sport-title">
+          <Modal.Title>{sport?.name}</Modal.Title>
+        </Modal.Header>
 
-      <Modal.Body>
-        <div className="sport-detail-content">
-          <img
-            src={sport?.image}
-            alt={sport?.name}
-            className="sport-detail-image"
-          />
+        <Modal.Body>
+          <div className="sport-detail-content">
+            <img
+              src={sport?.imageURL}
+              alt={sport?.name}
+              className="sport-detail-image"
+            />
 
-          <h4>Horarios de Entrenamiento</h4>
-          <Table striped bordered hover className="sport-schedule">
-            <thead>
-              <tr>
-                <th>Día</th>
-                <th>Horario</th>
-              </tr>
-            </thead>
-            <tbody>
-              {sport?.schedule.map((day, index) => (
-                <tr key={index}>
-                  <td>{day.day}</td>
-                  <td>{day.time}</td>
+            <h4>Horarios de Entrenamiento</h4>
+            <Table striped bordered hover className="sport-schedule">
+              <thead>
+                <tr>
+                  <th>Día</th>
+                  <th>Horario</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
 
-          <h4>Descripción</h4>
-          <ul className="sport-detail-list">
-            {sport?.description.map((desc, index) => (
-              <li key={index}>{desc}</li>
-            ))}
-          </ul>
-        </div>
-      </Modal.Body>
+            </Table>
 
-      <div className="btn-sport">
+            <h4>Descripción</h4>
+            <ul className="sport-detail-list">
 
-        {user && user.role === "Member" && (
+            </ul>
+          </div>
+        </Modal.Body>
+
+        <div className="btn-sport">
+          {user && user.role === "Member" && (
+            <Modal.Footer>
+              <Button className="btn-Incribirme" onClick={handleInscripcion}>
+                Incribirme
+              </Button>
+            </Modal.Footer>
+          )}
+
           <Modal.Footer>
-            <Button className="btn-Incribirme">
-              Incribirme
+            <Button className="btn-cerrar" onClick={onClose}>
+              Cerrar
             </Button>
           </Modal.Footer>
-        )}
+        </div>
+      </Modal>
 
+      <Modal show={showConfirmation} onHide={handleCloseConfirmation} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>¡Inscripción Exitosa!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Te has inscrito exitosamente en {sport?.name}.</Modal.Body>
         <Modal.Footer>
-          <Button className="btn-cerrar" onClick={onClose}>
+          <Button variant="success" onClick={handleCloseConfirmation}>
             Cerrar
           </Button>
         </Modal.Footer>
-      </div>
-    </Modal>
+      </Modal>
+    </>
   );
 };
 
--- React Frontend/Club-Deportivo/src/components/sports/Sports.jsx
+++ React Frontend/Club-Deportivo/src/components/sports/Sports.jsx
@@ -1,103 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Sports.css";
 import { Button } from "react-bootstrap";
 import SportDetail from "../sportDetail/SportDetail";
 
 const Sports = () => {
-
+  const [sport, setSport] = useState([]);
   const [selectedSport, setSelectedSport] = useState(null);
 
-  const sportsList = [
-    {
-      id: 1,
-      name: "Basquet",
-      description: [
-        "Lugar de entrenamiento: Cancha de basquet",
-        "Entrenador: Juan Pérez",
-      ],
-      image:
-        "http://webipedia.es/wp-content/uploads/2020/11/06_PelotaEntrandoACanasta.jpg",
-      schedule: [
-        { day: 'Lunes', time: '10:00 - 12:00' },
-        { day: 'Miércoles', time: '14:00 - 16:00' },
-        { day: 'Viernes', time: '16:00 - 18:00' },
-      ],
-    },
-    {
-      id: 2,
-      name: "Voley",
-      description: [
-        "Lugar de entrenamiento: Cancha de voley",
-        "Entrenadora: Sofia Gonzalez",
-      ],
-      image:
-        "https://1.bp.blogspot.com/-F0PamBjTPXY/UZtg4uUZG3I/AAAAAAAACRI/6QIdNWnUeuA/s1600/Annerys-Victoria-Vargas-Valdez-Volleyball-London-2012-Olympics.jpg",
-      schedule: [
-        { day: 'Martes', time: '10:00 - 12:00' },
-        { day: 'Jueves', time: '14:00 - 16:00' },
-      ],
-    },
-    {
-      id: 3,
-      name: "Tenis",
-      description: [
-        "Lugar de entrenamiento: Cancha de tenis",
-        "Entrenador: Pablo Ruiz",
-      ],
-      image: "https://deportivoromeral.cl/images/ramas/tenis2.jpg",
-      schedule: [
-        { day: 'Lunes', time: '10:00 - 12:00' },
-        { day: 'Miércoles', time: '14:00 - 16:00' },
-        { day: 'Viernes', time: '16:00 - 18:00' },
-      ],
-    },
-    {
-      id: 4,
-      name: "Futsal",
-      description: [
-        "Lugar de entrenamiento: Estadio",
-        "Entrenador: Esteban Gutierrez",
-      ],
-      image:
-        "https://www.timbo.sc.gov.br/wp-content/uploads/2018/11/futsal-divulgacao.jpg",
-      schedule: [
-        { day: 'Lunes', time: '10:00 - 12:00' },
-        { day: 'Miércoles', time: '14:00 - 16:00' },
-        { day: 'Viernes', time: '16:00 - 18:00' },
-      ],
-    },
-    {
-      id: 5,
-      name: "Natación",
-      description: [
-        "Lugar de entrenamiento: Pileta cubierta",
-        "Entrenador: María Gómez",
-      ],
-      image:
-        "https://media.revistagq.com/photos/5d15d8da274222b14be3327b/16:9/w_2560%2Cc_limit/gentrit-sylejmani-JjUyjE-oEbM-unsplash.jpg",
-      schedule: [
-        { day: 'Lunes', time: '10:00 - 12:00' },
-        { day: 'Miércoles', time: '14:00 - 16:00' },
-        { day: 'Viernes', time: '16:00 - 18:00' },
-      ],
-    },
-    {
-      id: 6,
-      name: "Hockey",
-      description: [
-        "Lugar de entrenamiento: Cancha de hockey",
-        "Entrenadora: Juan Perez"
-      ],
-      image:
-        "https://images.pexels.com/photos/163526/field-hockey-player-girls-game-163526.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      schedule: [
-        { day: 'Lunes', time: '10:00 - 12:00' },
-        { day: 'Miércoles', time: '14:00 - 16:00' },
-        { day: 'Viernes', time: '16:00 - 18:00' },
-      ],
-    },
-  ];
+  const fetchSport = async () => {
+    try {
+      const response = await fetch("https://localhost:7081/api/Sports/sports",{
+        method: "GET",
+        headers: {
+          accept: "*/*",
+          "Content-Type": "application/json",
+        },
+      })
+      if(response.ok){
+        const data = await response.json();
+        setSport(data);
+      } else {
+        throw new Error("Error al obtener las noticias");
+      }
+    } catch (error) {
+      console.error("Error:", error)
+    }
+  }
 
+  useEffect(() =>{
+    fetchSport();
+  }, [])
+  
   const handleShowDetail = (sport) => {
     setSelectedSport(sport);
   };
@@ -110,9 +43,9 @@ const Sports = () => {
     <div className="sports-container">
       <h2 className="sports-title">Nuestros Deportes</h2>
       <div className="sports-grid">
-        {sportsList.map((sport) => (
+        {sport.map((sport) => (
           <div key={sport.id} className="sport-card">
-            <img src={sport.image} alt={sport.name} className="sport-image" />
+            <img src={sport.imageURL} alt={sport.name} className="sport-image" />
             <h3 className="sport-name">{sport.name}</h3>
             <Button className="btn-ver-mas" onClick={() => handleShowDetail(sport)}>
               VER MÁS  <i className="fas fa-arrow-right"></i>
