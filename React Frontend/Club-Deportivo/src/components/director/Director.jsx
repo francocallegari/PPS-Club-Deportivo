7e2d7d913259556926ecabc01ade7e355612ffd3
Delfina Isaguirre <delfiisaguirre26@gmail.com>
2024-11-07 11:59:15 -0300

    director

--- React Frontend/Club-Deportivo/src/components/director/Director.jsx
+++ React Frontend/Club-Deportivo/src/components/director/Director.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import "./Director.css";
-import UsersList from '../usersList/UsersList';
 
 const Director = () => {
     const initialEvents = [
@@ -13,6 +12,7 @@ const Director = () => {
     const [events, setEvents] = useState(initialEvents);
     const [showModal, setShowModal] = useState(false);
     const [selectedEvent, setSelectedEvent] = useState(null);
+    const [users, setUsers] = useState([]);
 
     const handleShowModal = (event) => {
         setSelectedEvent(event);
@@ -34,21 +34,108 @@ const Director = () => {
         }
     };
 
+    const handleDeleteUser = async (userId) => {
+        try {
+            const response = await fetch(`https://localhost:7081/api/User/${userId}`, {
+                method: 'DELETE',
+            });
+
+            if (!response.ok) {
+                throw new Error('Error al eliminar el usuario');
+            }
+
+            setUsers(users.filter(user => user.id !== userId));
+        } catch (error) {
+            console.error("Error deleting user:", error);
+        }
+    }
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const response = await fetch("https://localhost:7081/api/User")
+                if (!response.ok) throw new Error('Error al obtener los usuarios');
+                const data = await response.json();
+                setUsers(data);
+            } catch (error) {
+                console.error("Error fetching users:", error)
+            }
+        }
+
+        fetchUser();
+    }, []);
+
     return (
         <>
-            <h3 className='evento-approve'>Usuarios</h3>
+            <h3 className="evento-approve">Usuarios</h3>
             <div className="user-container">
                 <div className="user-column">
-                    <h3 className='user-title'>Administradores</h3>
-                    <UsersList option="admins"></UsersList>
+                    <h3 className="user-title">Socios</h3>
+                    {users.length > 0 ? (
+                        users
+                            .filter((user) => user?.userType === "Member")
+                            .map((user) => (
+                                <p key={user.id} className="user">
+                                    {user.userName} <i
+                                        className="fa-solid fa-x"
+                                        onClick={() => handleDeleteUser(user.id)}
+                                    ></i>
+                                </p>
+                            ))
+                    ) : (
+                        <p>Cargando socios...</p>
+                    )}
                 </div>
+
                 <div className="user-column">
-                    <h3 className='user-title'>Entrenadores</h3>
-                    <UsersList option="coaches"></UsersList>
+                    <h3 className="user-title">Administradores</h3>
+                    {users.length > 0 ? (
+                        users
+                            .filter((user) => user?.userType === "Admin")
+                            .map((user) => (
+                                <p key={user.id} className="user">
+                                    {user.userName} <i
+                                        className="fa-solid fa-x"
+                                        onClick={() => handleDeleteUser(user.id)}
+                                    ></i>
+                                </p>
+                            ))
+                    ) : (
+                        <p>Cargando administradores...</p>
+                    )}
                 </div>
+
                 <div className="user-column">
-                    <h3 className='user-title'>Socios</h3>
-                    <UsersList option="members"></UsersList>
+                    <h3 className="user-title">Entrenadores</h3>
+                    {users.length > 0 ? (
+                        users
+                            .filter((user) => user?.userType === "Coach")
+                            .map((user) => (
+                                <p key={user.id} className="user">
+                                    {user.userName} <i
+                                        className="fa-solid fa-x"
+                                        onClick={() => handleDeleteUser(user.id)}
+                                    ></i>
+                                </p>
+                            ))
+                    ) : (
+                        <p>Cargando entrenadores...</p>
+                    )}
+                </div>
+
+                <div className="user-column">
+                    <h3 className="user-title">Directores</h3>
+                    {users.length > 0 ? (
+                        users
+                            .filter((user) => user?.userType === "Director")
+                            .map((user) => (
+                                <p key={user.id} className="user">
+                                    {user.userName}
+                                </p>
+                            ))
+                    ) : (
+                        <p>Cargando directores...</p>
+                    )}
                 </div>
             </div>
 
