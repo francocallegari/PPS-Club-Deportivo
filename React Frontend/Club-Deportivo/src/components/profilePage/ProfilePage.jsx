83d274f3eeb814907c27c4a6eee7f2c068f6dad2
ManueldeMacedo <75130351+ManueldeMacedo@users.noreply.github.com>
2024-11-07 22:57:55 -0300

    Revert "Upload of personal data added"
    This reverts commit 5136022310940dd5ba29ef23c97eada407390e66.

--- React Frontend/Club-Deportivo/src/components/profilePage/ProfilePage.jsx
+++ React Frontend/Club-Deportivo/src/components/profilePage/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Button, Modal, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./ProfilePage.css";
@@ -13,71 +13,10 @@ import {
 
 const ProfilePage = () => {
   const [show, setShow] = useState(false);
-  const [profileData, setProfileData] = useState(null);
   const { user } = useContext(AuthenticationContext);
-
-
-  useEffect(() => {
-    const fetchProfileData = async () => {
-      try {
-        const response = await fetch(`https://localhost:7081/api/User/${user.id}`, {
-          method: "GET",
-          mode: 'cors',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-      });
-        const data = await response.json();
-        setProfileData(data);
-      } catch (error) {
-        console.error("Error fetching profile data:", error);
-      }
-    };
-
-    fetchProfileData();
-  }, []);
-
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  if (!profileData) {
-    return <p>Loading...</p>;
-  }
-
-  address
-: 
-"Calle Santa Fe 5678, Rosario, Santa Fe, Argentina"
-dateOfBirth
-: 
-"1985-03-22"
-dni
-: 
-"21234567"
-email
-: 
-"tom.brown@club.com"
-id
-: 
-5
-name
-: 
-"Tom Brown"
-phoneNumber
-: 
-"341-567-8901"
-userDeletionDate
-: 
-null
-userName
-: 
-"member_tom"
-userRegistrationDate
-: 
-"0001-01-01T00:00:00"
-userType
-: 
-"Member"
 
   return (
     <div className="socio-container">
@@ -90,23 +29,26 @@ userType
           <Form>
             <Form.Group controlId="formDocumentNumber">
               <Form.Label>Documento</Form.Label>
-              <Form.Control type="text" defaultValue={profileData.dni} />
+              <Form.Control type="text" defaultValue="36859884" />
             </Form.Group>
             <Form.Group controlId="formEmail">
               <Form.Label>Dirección de correo electrónico</Form.Label>
-              <Form.Control type="email" defaultValue={profileData.email} />
+              <Form.Control type="email" defaultValue="alexa@example.com" />
             </Form.Group>
             <Form.Group controlId="formPhone">
               <Form.Label>Teléfono de contacto</Form.Label>
-              <Form.Control type="text" defaultValue={profileData.phoneNumber} />
+              <Form.Control type="text" defaultValue="341 3225849" />
             </Form.Group>
             <Form.Group controlId="formAddress">
               <Form.Label>Dirección</Form.Label>
-              <Form.Control type="text" defaultValue={profileData.address} />
+              <Form.Control
+                type="text"
+                defaultValue="Zeballos 1059, Rosario, Santa Fe"
+              />
             </Form.Group>
             <Form.Group controlId="formBirthdate">
               <Form.Label>Fecha de nacimiento</Form.Label>
-              <Form.Control type="date" defaultValue={profileData.dateOfBirth} />
+              <Form.Control type="date" defaultValue="1986-10-14" />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -130,7 +72,7 @@ userType
           alt="Foto de perfil"
           className="foto-perfil"
         />
-        <h2 className="nombre-usuario">{profileData.name}</h2>
+        <h2 className="nombre-usuario">Alexa Rodríguez</h2>
 
         <Button
           className="boton-modificar"
@@ -144,12 +86,30 @@ userType
       <div className="info-personal">
         <h3 className="section-title">Mis datos personales</h3>
         <div className="datos-grid">
-        <p><label>Nº de socio:</label> {profileData.id}</p>
-          <p><label>Documento:</label> {profileData.dni}</p>
-          <p><label>Dirección de correo electrónico:</label> {profileData.email}</p>
-          <p><label>Teléfono de contacto:</label> {profileData.phoneNumber}</p>
-          <p><label>Dirección:</label> {profileData.address}</p>
-          <p><label>Fecha de nacimiento:</label> {profileData.dateOfBirth}</p>
+          <p>
+            <label>Nº de socio:</label>
+            53048
+          </p>
+          <p>
+            <label>Documento:</label>
+            36859884
+          </p>
+          <p>
+            <label>Dirección de correo electrónico:</label>
+            alexa@example.com
+          </p>
+          <p>
+            <label>Teléfono de contacto:</label>
+            341 3225849
+          </p>
+          <p>
+            <label>Dirección:</label>
+            Zeballos 1059, Rosario, Santa Fe
+          </p>
+          <p>
+            <label>Fecha de nacimiento:</label>
+            14 de octubre de 1986
+          </p>
         </div>
       </div>
 
@@ -170,14 +130,14 @@ userType
       </div>
 
       {user && user.role === "Member" && (
-        <div className="deportes">
-          <h3 className="section-title">Deportes y actividades</h3>
-          <FontAwesomeIcon icon={faVolleyballBall} /> Voley
-          <h1></h1>
-          <FontAwesomeIcon icon={faFutbol} /> Fútbol
-          <h1></h1>
-          <FontAwesomeIcon icon={faSwimmer} /> Natación
-        </div>
+      <div className="deportes">
+        <h3 className="section-title">Deportes y actividades</h3>
+        <FontAwesomeIcon icon={faVolleyballBall} /> Voley
+        <h1></h1>
+        <FontAwesomeIcon icon={faFutbol} /> Fútbol
+        <h1></h1>
+        <FontAwesomeIcon icon={faSwimmer} /> Natación
+      </div>
       )}
 
       {user && user.role === "Member" && (
